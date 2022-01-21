3808f9cfdf52bf09c7598a6aa590151507ee91f1
Julio <julionev10@gmail.com>
2022-01-21 13:02:37 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -37,7 +37,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-        <StatusBar barStyle="light-content" />
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor='transparent'
+          translucent 
+        />
         <AuthProvider>
           <Routes />
         </AuthProvider>
